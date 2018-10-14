4a0169c208dab2a2bc7672ff8eca90c51ae217a0
ormxmi <36085168+ormxmi@users.noreply.github.com>
2018-10-14 21:08:37 +0300

    added colors

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ document.getElementById("suob6tenie_vuvedi").addEventListener('keypress', functi
     }
 });
 var lastMessageMine = true;
+var pulledDownMenu = false;
 function sendMsg(){
 
     var msg = document.getElementById("suob6tenie_vuvedi").value;
@@ -34,3 +35,51 @@ function sendMsg(){
 }
     console.log(msg);
 };
+
+function pullDownMenu(e){
+    var dropDownMenu = document.getElementsByClassName("overlay")[0];
+    //document.getElementsByClassName("overlayArrow")[0].style.transform = "rotateZ(1080deg);";
+    if(e=="close"){
+        dropDownMenu.style.transform = "translateY(-85%)";
+        pulledDownMenu = false;
+    }else{
+        if(!pulledDownMenu){
+            dropDownMenu.style.transform = "translateY(0%)";
+            pulledDownMenu = true;
+        }
+        else{
+            dropDownMenu.style.transform = "translateY(-85%)";
+            pulledDownMenu = false;
+        }
+    }
+}
+
+function changeColorsDrop(e){
+    document.getElementsByClassName("chooseColorsOutline")[0].style.top = e;
+}
+let lastColorsId = 0;
+function changeColors(colors){
+    console.log(colors);
+    console.log( document.getElementById("sendBtn"));
+    document.getElementsByClassName('chooseColors')[lastColorsId].setAttribute("class","chooseColors");
+    document.getElementsByClassName('chooseColors')[colors[2]].setAttribute("class","chooseColors chosen");
+    lastColorsId = colors[2];
+    document.getElementById("sendBtn").style.backgroundColor = colors[1];
+    document.getElementById("sendBtn").style.color = colors[0];
+    if(colors[1]=="pink")document.getElementById("sendBtn").style.color =colors[2];
+    var senderLength = document.querySelectorAll('.send').length;
+    var ownLength = document.querySelectorAll('.own').length;
+    console.log(senderLength);console.log(ownLength);
+    for(let i =0;i<senderLength;i++){
+        
+    
+        
+    document.getElementsByClassName("send")[i].style.backgroundColor = colors[1];
+    document.getElementsByClassName("send")[i].style.color = colors[3];
+    }
+    for(let v =0;v<ownLength;v++){
+       
+    document.getElementsByClassName("own")[v].style.backgroundColor = colors[0];
+        }
+    
+}
