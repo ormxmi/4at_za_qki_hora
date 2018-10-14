8ce4448541beea6fe1e21ca994fd6a3d4a1c8226
ormxmi <36085168+ormxmi@users.noreply.github.com>
2018-10-14 18:21:08 +0300

    added names

--- main.js
+++ main.js
@@ -3,15 +3,34 @@ document.getElementById("suob6tenie_vuvedi").addEventListener('keypress', functi
         sendMsg();
     }
 });
+var lastMessageMine = true;
 function sendMsg(){
+
     var msg = document.getElementById("suob6tenie_vuvedi").value;
+    while(msg[0]==" ")
+    {
+        msg = msg.substr(1);
+    }
     if(msg!==""){
-    document.getElementById("suob6tenie_vuvedi").value = "";
-    var newMsg = document.createElement("P");
-    newMsg.setAttribute("class","message dqsno");
-    var newMsgNode = document.createTextNode(msg);
-    newMsg.appendChild(newMsgNode);
-    document.getElementsByClassName("actualChat")[0].appendChild(newMsg);
+        var div_messageContainer = document.createElement("DIV");
+        div_messageContainer.setAttribute("class","messageContainer dqsno");
+            if(!lastMessageMine){
+                var newName = document.createElement("P");
+                newName.setAttribute("class","senderName");
+                var newNameNode = document.createTextNode("Me");
+                newName.appendChild(newNameNode); 
+                div_messageContainer.appendChild(newName);
+
+                lastMessageMine = true
+            }
+            
+        document.getElementById("suob6tenie_vuvedi").value = "";
+        var newMsg = document.createElement("P");
+        newMsg.setAttribute("class","message lightgray");
+        var newMsgNode = document.createTextNode(msg);
+        newMsg.appendChild(newMsgNode);
+        div_messageContainer.appendChild(newMsg);
+        document.getElementsByClassName("actualChat")[0].appendChild(div_messageContainer);
 }
     console.log(msg);
 };
